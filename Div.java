ea5ae659a613e83e8c6212ecafde210010479d49
Meda Castaian <meda.castaian@gmail.com>
2017-08-20 19:06:38 +0300

    Exercise-03

---
+++ Div.java
@@ -0,0 +1,9 @@
+package com.edocti.jintro.lab01;
+public class Div { 
+    public static void main(String[] args) {
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double div  = a / b;
+        System.out.println(a + " / " + b + " = " + div);
+    }
+}
