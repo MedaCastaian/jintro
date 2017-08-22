939e21cd87911a4e4f09d1df68297b668153110d
MedaCastaian <30568804+MedaCastaian@users.noreply.github.com>
2017-08-22 14:51:43 +0300

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,9 +1,9 @@
 package com.edocti.jintro.lab01;
-	public class Arrays {
-		public static void main(String[] args) {
-			String[]names = {"Ion","Maria", "Elena", "Oana"};
-			for(int i = 0; i < names.length; i++) {
-				System.out.println(names[i].toUpperCase());
-			}
+public class Arrays {
+	public static void main(String[] args) {
+		String[]names = {"Ion","Maria", "Elena", "Oana"};
+		for(int i = 0; i < names.length; i++) {
+			System.out.println(names[i].toUpperCase());
 		}
 	}
+}
