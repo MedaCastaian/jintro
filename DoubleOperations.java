9e15e21e16f1fb07486bd1acb9324d695f5e5b44
MedaCastaian <30568804+MedaCastaian@users.noreply.github.com>
2017-08-22 14:50:13 +0300

    Update DoubleOperations.java

--- DoubleOperations.java
+++ DoubleOperations.java
@@ -2,7 +2,7 @@ package com.edocti.jintro.lab01;
 public class DoubleOperations{
 	public static void main(String... args){
 		double a = Double.parseDouble(args[0]);
-		a=Math.toRadians(a);
+		a = Math.toRadians(a);
 		System.out.println("Math.sin(" + a + ") = " + Math.sin(a));
 	}
 }
