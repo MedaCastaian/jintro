61362950d139b5c5ea135d56e87eea56b5fdd150
MedaCastaian <30568804+MedaCastaian@users.noreply.github.com>
2017-08-22 15:50:25 +0300

    Update DemoBoolean.java

--- DemoBoolean.java
+++ DemoBoolean.java
@@ -1,10 +1,11 @@
 package com.edocti.jintro.lab01;
 public class DemoBoolean {
-	public static void main(String[] args){
-		boolean isOld=(age > 65);
+	public static void main(String[] args) {
 		int age = Integer.parseInt(args[0]);
-		if(isOld){
+		boolean isOld = varsta > OLD_AGE;
+		if(isOld) {
 			System.out.println("You are old!");
-		}	
-	}
-}
+		} 
+	} 
+	private static final int OLD_AGE = 65;
+} 
