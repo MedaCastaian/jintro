0b53d293d57b0dc0b304661ad6ab1be01a080e60
MedaCastaian <30568804+MedaCastaian@users.noreply.github.com>
2017-08-22 19:16:29 +0300

    Update Div2.java

--- Div2.java
+++ Div2.java
@@ -7,11 +7,11 @@ public class Div2 {
 		c = 10;
 		int x, y;
 		x = 20;
-		y=0;
-		System.out.println(a/b); //Infinty
-		System.out.println(a/b - c/b); //NaN
-		System.out.println((a/b - c/b) * 200); //NaN
-		System.out.println((int)((a/b - c/b) * 200)); //cand fac cast la int, NaN (care este o valoare speciala Float) este transformat in zero no matter what
-		System.out.println(x/y); //Ultima linie da Division by zero (ArithmeticException), pentru ca numerele nu mai sunt float, ci int. Aici nu avem aceleasi reguli ca si la float. Daca imparti un intreg la zero, programul va crapa.
+		y = 0;
+		System.out.println(a / b); //Infinty
+		System.out.println(a / b - c / b); //NaN
+		System.out.println((a / b - c / b) * 200); //NaN
+		System.out.println((int)((a / b - c / b) * 200)); //cand fac cast la int, NaN (care este o valoare speciala Float) este transformat in zero no matter what
+		System.out.println(x / y); //Ultima linie da Division by zero (ArithmeticException), pentru ca numerele nu mai sunt float, ci int. Aici nu avem aceleasi reguli ca si la float. Daca imparti un intreg la zero, programul va crapa.
 	}
-}
+} 
