a379af7097718a57114478907944d8d32f432461
Paul Ianas <pianas@gmail.com>
2017-08-23 14:23:26 +0300

    Update Arrays.java

--- com/edocti/jintro/lab01/Arrays.java
+++ com/edocti/jintro/lab01/Arrays.java
@@ -1,7 +1,7 @@
 package com.edocti.jintro.lab01;
 public class Arrays {
 	public static void main(String[] args) {
-		String[]names = {"Ion","Maria", "Elena", "Oana"};
+		String[] names = {"Ion","Maria", "Elena", "Oana"};
 		for(int i = 0; i < names.length; i++) {
 			System.out.println(names[i].toUpperCase());
 		}
