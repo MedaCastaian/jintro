bf85c1027c64be0a1d5845168bdac4b3a0318514
MedaCastaian <30568804+MedaCastaian@users.noreply.github.com>
2017-08-23 12:18:37 +0300

    Update Div.java

--- com/edocti/jintro/lab01/Div.java
+++ com/edocti/jintro/lab01/Div.java
@@ -1,5 +1,9 @@
 package com.edocti.jintro.lab01;
 public class Div { 
+        inf (args.length < 2) {
+            System.err.println("Usage: java com.edocti.jintro.lab01.Div n1 n2");
+            System.exit(-1);
+        }
     public static void main(String[] args) {
         double a = Double.parseDouble(args[0]);
         double b = Double.parseDouble(args[1]);
