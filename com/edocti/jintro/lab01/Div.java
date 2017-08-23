0f660ab7e8762a337a3d63f64f9894aaad51ca70
Paul Ianas <pianas@gmail.com>
2017-08-23 14:22:59 +0300

    Something happened when you merged
    The if block was outside any method. This is not working.

--- com/edocti/jintro/lab01/Div.java
+++ com/edocti/jintro/lab01/Div.java
@@ -1,10 +1,10 @@
 package com.edocti.jintro.lab01;
-public class Div { 
+public class Div {
+    public static void main(String[] args) {
         inf (args.length < 2) {
             System.err.println("Usage: java com.edocti.jintro.lab01.Div n1 n2");
             System.exit(-1);
         }
-    public static void main(String[] args) {
         double a = Double.parseDouble(args[0]);
         double b = Double.parseDouble(args[1]);
         double div  = a / b;
