34fec0f9e4dbbfcaa3f39bf2f9049b320012c8ef
Meda Castaian <meda.castaian@gmail.com>
2017-08-27 17:24:49 +0300

    Exercise

---
+++ com/edocti/Connection.java
@@ -0,0 +1,10 @@
+package com.edocti.jintro;
+import com.edocti.jintro.lab01.Test;
+
+public class Connection {
+	public static void main(String[] args) {
+		System.out.println("Hello from package");
+		Test.print("Cucu");
+		com.edocti.jintro.lab02.Test.print("Cucu");
+	}
+} 
