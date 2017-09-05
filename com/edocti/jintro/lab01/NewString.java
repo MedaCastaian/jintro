3b3889e3954fbaab05c8c4bf9a0922e188706d21
MedaCastaian <30568804+MedaCastaian@users.noreply.github.com>
2017-09-05 09:36:35 +0300

    Update NewString.java

--- com/edocti/jintro/lab01/NewString.java
+++ com/edocti/jintro/lab01/NewString.java
@@ -1,10 +1,10 @@
 package com.edocti.jintro.lab01;
 public class NewString {
-	public static void main(String[] args) {
-		String a, b, c;
-		a = "My name is "; 
-		b = "Meda";
-		c = a + b;
-		System.out.println(c);
-	}
+    public static void main(String[] args) {
+        String a, b, c;
+        a = "My name is "; 
+        b = "Meda";
+        c = a + b;
+        System.out.println(c);
+    }
 } 
