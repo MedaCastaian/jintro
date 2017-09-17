f1a5209473a6e87455e7d277faf009f14b704be3
Meda Castaian <30568804+MedaCastaian@users.noreply.github.com>
2017-09-17 17:17:52 +0300

    Update ShuffleArray.java

--- com/edocti/jintro/lab02/ShuffleArray.java
+++ com/edocti/jintro/lab02/ShuffleArray.java
@@ -2,10 +2,10 @@ package com.edocti.jintro.lab02;
 import java.lang.Math;
 public class ShuffleArray {
 	
-	private static void shuffleArray(int[] a) {
-		int N = a.length;
+    private static void shuffleArray(int[] a) {
+	int N = a.length;
         // between i and N-1
-		for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++) {
             int r = (int) (N * Math.random());
         int tmp = a[i];    // swap
         a[i] = a[r];
@@ -14,10 +14,10 @@ public class ShuffleArray {
     }
 
     public static void main(String[] args) {
-	    int[] a = new int[] {1, 4, 20, 10, 6, 7, 9};
-		shuffleArray(a);
-		for (int i: a) {
-		    System.out.print(i + " ");
-		}
+        int[] a = new int[] {1, 4, 20, 10, 6, 7, 9};
+	shuffleArray(a);
+	for (int i: a) {
+	    System.out.print(i + " ");
 	}
+    }
 }
