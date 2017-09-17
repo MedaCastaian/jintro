99b8cba4119824c9339290a5d2cb7090d4a95fc8
Meda Castaian <30568804+MedaCastaian@users.noreply.github.com>
2017-09-17 17:08:33 +0300

    Update IntegerOperations.java

--- com/edocti/jintro/lab02/IntegerOperations.java
+++ com/edocti/jintro/lab02/IntegerOperations.java
@@ -4,21 +4,21 @@ public class IntegerOperations {
 
     private static void validateArguments(String[] args) {
         if (args.length < 3) {
-	err.println("Usage: IntegerOperations int1 int2 operators");
-	throw new IllegalArgumentException("Usage: IntegerOperations int1 int2 operators");
-	//exit(-1);
+	    err.println("Usage: IntegerOperations int1 int2 operators");
+	    throw new IllegalArgumentException("Usage: IntegerOperations int1 int2 operators");
+	    //exit(-1);
 	}
     }
 	
     private static int readInt(String input) {
 	if (stringIsEmpty(input)) {
-	err.println("Invalid number:" + input);
-	return Integer.MIN_VALUE; // without Exceptions
+	    err.println("Invalid number:" + input);
+	    return Integer.MIN_VALUE; // without Exceptions
 	}
 	try {
-		return Integer.parseInt(input);
+	    return Integer.parseInt(input);
 	} catch (NumberFormatException e) {
-		return Integer.MIN_VALUE; // cea ma mica valoare reprezentata pe 32 de biti ca si un intreg
+	    return Integer.MIN_VALUE; // cea ma mica valoare reprezentata pe 32 de biti ca si un intreg
 	}
     }
 	
@@ -32,34 +32,34 @@ public class IntegerOperations {
 	    return Integer.MIN_VALUE;
 	}
 	return a / b;
-	}
+    }
 	
     private static int modulo(int a, int b) {
 	if (b == 0) {
-	err.println("Modulo by zero!");
-		return Integer.MIN_VALUE;
+	    err.println("Modulo by zero!");
+	    return Integer.MIN_VALUE;
 	}
 	return a % b;
     }
 	
     public static void main(String[] args) {
-    validateArguments(args);
-    int a = readInt(args[0]);
-    int b = readInt(args[1]);
-    String operation = args[2].toLowerCase();
-    int result = Integer.MIN_VALUE;
+        validateArguments(args);
+        int a = readInt(args[0]);
+        int b = readInt(args[1]);
+        String operation = args[2].toLowerCase();
+        int result = Integer.MIN_VALUE;
 		
-    if (operation.equals("add")) {
-	result = a + b;
-    } else if (operation.equals("prod")) {
-	result = a * b;
-    } else if (operation.equals("div")) {
-	result = divide(a, b);
-    } else if (operation.equals("mod")) {
-	result = modulo(a, b);
+        if (operation.equals("add")) {
+	    result = a + b;
+        } else if (operation.equals("prod")) {
+	    result = a * b;
+        } else if (operation.equals("div")) {
+	    result = divide(a, b);
+        } else if (operation.equals("mod")) {
+	    result = modulo(a, b);
+        }
+            err.println("Unknown operation");	
+            out.printf("%d %s %d = %d\n", a, operation, b, result);
     }
-        err.println("Unknown operation");	
-        out.printf("%d %s %d = %d\n", a, operation, b, result);
-   }
 } 
 		
