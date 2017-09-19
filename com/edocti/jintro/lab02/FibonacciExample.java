1dd361cbba378e5c625c41b55f55801eb3e3ca69
Meda Castaian <30568804+MedaCastaian@users.noreply.github.com>
2017-09-19 20:47:19 +0300

    Update FibonacciExample.java

--- com/edocti/jintro/lab02/FibonacciExample.java
+++ com/edocti/jintro/lab02/FibonacciExample.java
@@ -3,10 +3,10 @@ public class FibonacciExample {
     
     public static int fibonacci(int n) {
         if (n == 0 || n == 1) {
-        return n;
+            return n;
         }
         else {
-        return fibonacci(n-1) + fibonacci(n-2);
+            return fibonacci(n-1) + fibonacci(n-2);
         }
     }
 	
