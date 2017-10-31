33529373a141b2d7a2ae5f1a526a1dfbf555de1e
Meda Castaian <30568804+MedaCastaian@users.noreply.github.com>
2017-10-31 19:00:14 +0200

    Update Demo5.java

--- com/edocti/jintro/lab02/Demo5.java
+++ com/edocti/jintro/lab02/Demo5.java
@@ -5,12 +5,12 @@ package com.edocti.jintro.lab02;
 import java.util.*;
 public class Demo5 {
     public static void main(String[] args) {
-        float studentIDs[] = new float [50];
+        float[] studentIDs = new float [30];
         Arrays.fill(studentIDs, 1);
-        for(int i = 0; i < studentIDs.length; i++) {
+        for (int i = 0; i < studentIDs.length; i++) {
             System.out.print(studentIDs [i]);
         }
-        Arrays.fill(studentIDs, 20, 31, 0);
+        Arrays.fill(studentIDs, 10, 20, 0);
         for(int i = 0; i < studentIDs.length; i++) {
             System.out.print(studentIDs [i]);
         }
