6b632c085e9899c366d5346abc88559a968171ab
Meda Castaian <30568804+MedaCastaian@users.noreply.github.com>
2017-10-31 19:20:32 +0200

    Update Demo4.java

--- com/edocti/jintro/lab02/Demo4.java
+++ com/edocti/jintro/lab02/Demo4.java
@@ -5,7 +5,7 @@ package com.edocti.jintro.lab02;
 import java.util.*;
 public class Demo4 {
     public static void main(String[] args) {
-        int studentIDs[] = new int [50];
+        int[] studentIDs = new int [50];
         Arrays.fill(studentIDs, 1);
         for(int i = 0; i < studentIDs.length; i++) {
             System.out.print(studentIDs [i]);
