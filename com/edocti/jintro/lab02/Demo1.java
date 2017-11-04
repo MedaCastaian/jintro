afdbb38dae3617bb259f60dac1f9194978b53e6a
Meda Castaian <30568804+MedaCastaian@users.noreply.github.com>
2017-11-04 17:37:55 +0200

    Update Demo1.java

--- com/edocti/jintro/lab02/Demo1.java
+++ com/edocti/jintro/lab02/Demo1.java
@@ -5,12 +5,12 @@ package com.edocti.jintro.lab02;
 public class Demo1 {
     public static void main(String[] args) {
     	float grade;
-    	float rawTest[] = {70, 100};
+    	float rawTest[] = { 70, 100 };
     	grade = calculateGrade(rawTest);
     	System.out.println("Your grade is: " + grade);
     }
     
     private static float calculateGrade(float test[]) {
-    	return (test[0]/test[1]) * 100;
+    	return (test[0] / test[1]) * 100;
     }  
 }
