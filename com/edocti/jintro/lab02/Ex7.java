c17c96ddf0253de84c7f37875f64489931a6600c
Meda Castaian <30568804+MedaCastaian@users.noreply.github.com>
2017-11-04 18:41:51 +0200

    Update Ex7.java

--- com/edocti/jintro/lab02/Ex7.java
+++ com/edocti/jintro/lab02/Ex7.java
@@ -6,15 +6,15 @@ import java.text.*;
 public class Ex7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-	 double payment = scanner.nextDouble();
-	 scanner.close();
+	double payment = scanner.nextDouble();
+	scanner.close();
         
         String us = NumberFormat.getCurrencyInstance(new Locale("en","US")).format(payment).toString();
-	 String china = NumberFormat.getCurrencyInstance(new Locale("zh","CN")).format(payment).toString();
+	String china = NumberFormat.getCurrencyInstance(new Locale("zh","CN")).format(payment).toString();
         String france =NumberFormat.getCurrencyInstance(new Locale("fr","FR")).format(payment).toString();
 	
-	 System.out.println("US: " + us);
-	 System.out.println("China: " + china);
-	 System.out.println("France: " + france);
+	System.out.println("US: " + us);
+	System.out.println("China: " + china);
+	System.out.println("France: " + france);
     } 
 }
